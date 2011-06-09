bd1dec451f1e07da6a8d18e79eb39cb8596c4a1b
Hanno Jonas <hanno.jonas@gmail.com>
2011-06-09 13:57:50 +0200

    reverted to old version

--- Site/fb.php
+++ Site/fb.php
@@ -4,33 +4,25 @@ include_once('../Includes/Init.php');
 include_once('../Includes/Config.php');
 include_once('../Includes/Snippets.php');
 
-$myUrl = $GLOBALS['BASE_URL'] . 'Site/fb.php';
+$myUrl = 'http://notethrower.com/NTTest/Site/fb.php'; // FIXME #################
 
-$code    = get_param('code');
-$destUrl = get_param('destUrl');
+$code = $_REQUEST['code'];
 
-if (!$code) {
-    $dialogUrl = 'http://www.facebook.com/dialog/oauth?client_id=' .
-            $GLOBALS['FACEBOOK_APP_ID'] . '&redirect_uri=' . urlencode($myUrl) .
-            '&scope=email';
-            //'&destUrl=' . urlencode(get_param('destUrl')); // FIXME - reactivate
+if(empty($code)) {
+    $dialogUrl = "http://www.facebook.com/dialog/oauth?client_id=" .
+        $GLOBALS['FACEBOOK_APP_ID'] . "&redirect_uri=" . urlencode($myUrl) .
+        '&scope=email';
 
     redirectTo($dialogUrl);
 
 } else {
-    $tokenUrl = 'https://graph.facebook.com/oauth/access_token?client_id=' .
-            $GLOBALS['FACEBOOK_APP_ID'] . '&redirect_uri=' . urlencode($myUrl) . '&client_secret=' .
-            $GLOBALS['FACEBOOK_APP_SECRET'] . '&code=' . $code;
+    $tokenUrl = "https://graph.facebook.com/oauth/access_token?client_id="
+        . $GLOBALS['FACEBOOK_APP_ID'] . "&redirect_uri=" . urlencode($myUrl) . "&client_secret="
+        . $GLOBALS['FACEBOOK_APP_SECRET'] . "&code=" . $code;
 
     $accessToken = file_get_contents($tokenUrl);
 
-    if ($destUrl) {
-        $concatChar = strpos($destUrl, '?') !== false ? '&' : '?';
-        redirectTo($destUrl . $concatChar . $accessToken);
-
-    } else {
-        redirectTo($GLOBALS['BASE_URL'] . 'Site/index.php?' . $accessToken);
-    }
+    redirectTo('/NTTest/Site/startpageNew.php?' . $accessToken); // FIXME #################
 }
 
 ?>

8e96ba34015e33472b0ac5043db6e642000d497a
Hanno Jonas <hanno.jonas@gmail.com>
2011-10-11 10:33:09 +0200

    renaming artists.php to artistList.php - step 1

--- Site/mainMenu.php
+++ Site/mainMenu.php
@@ -1,15 +1,15 @@
 <div id="mainMenuWrapper">
     <div id="mainMenu">
     	<ul>
-    		<li><a href="indexNew.php">Artists</a></li>
+    		<li><a href="artistList.php">Artists</a></li>
     		<li><a href="blogNew.php">Blog</a></li>
     		<li><a href="featuresNew.php">Features</a></li>
     		<li><a href="indexNew.php">Plans</a></li>
     		<li><a href="indexNew.php">Store</a></li>
     		<li><a href="indexNew.php">Log In</a></li>
-    		<!-- 
+    		<!--
     		<li><a href="index.php">Genre</a></li>
-    		<li><a href="/">Community</a></li> 
+    		<li><a href="/">Community</a></li>
     		-->
     	</ul>
     </div>

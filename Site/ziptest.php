56abd86dd6382c1e7fed4fb6bcb9214be3203b85
Hanno Jonas <hanno.jonas@gmail.com>
2011-10-12 11:01:50 +0200

    tmp upd

--- Site/ziptest.php
+++ Site/ziptest.php
@@ -1,13 +1,16 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+
 $zip = new ZipArchive();
-$filename = "./test.zip";
+$filename = "/home/benso/oneloudr.com/OL/Content/UserImages/test.zip";
 
 if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
     exit("cannot open <$filename>\n");
 }
 
-$zip->addFile("./Content/UserImages/1d18a9b35a46b853bf62eb2114aacd56/1d18a9b35a46b853bf62eb2114aacd56.jpg", "./Content/UserImages/1d18a9b35a46b853bf62eb2114aacd56/1d18a9b35a46b853bf62eb2114aacd56_thumb.jpg");
+$zip->addFile("/home/benso/oneloudr.com/OL/Content/UserImages/1d18a9b35a46b853bf62eb2114aacd56/1d18a9b35a46b853bf62eb2114aacd56.jpg", "Files/img.jpg");
+$zip->addFile("/home/benso/oneloudr.com/OL/Content/UserImages/1d18a9b35a46b853bf62eb2114aacd56/1d18a9b35a46b853bf62eb2114aacd56_thumb.jpg", "Files/thumb.jpg");
 echo "numfiles: " . $zip->numFiles . "\n";
 echo "status:" . $zip->status . "\n";
 $zip->close();

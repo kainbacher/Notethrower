df801dbb3d76b74ef7961cf38e90365dcb8b3d51
mschreiber <martin@mschreiber.at>
2011-11-04 23:19:52 +0100

    adding transcoding stuff

---
+++ Includes/DB/TranscodingJob.php
@@ -0,0 +1,155 @@
+<?php
+
+include_once('../Includes/Config.php');
+include_once('../Includes/DbConnect.php');
+include_once('../Includes/Snippets.php');
+
+// dao for pp_transcoding_job table
+class TranscodingJob {
+    var $id;
+    var $projectFileId;
+    var $status; // PENDING; PROCESSING; SUCCEES; FAILED;
+    var $entryDate;
+    var $updatedDate;
+    var $info;
+
+    //no db table fields
+    var $projectId;
+    var $filename;
+
+    // constructors
+    // ------------
+    function TranscodingJob() {
+    }
+
+    function fetchPending() {
+        $objs = array();
+
+        $result = null;
+        $result = _mysql_query('select j.id, j.project_file_id, j.status, j.info, j.entry_date, ' .
+                               'j.updated_date, f.filename ' .
+                               'from pp_transcoding_job j left join pp_project_file f on f.id=j.project_file_id ' .
+                               'where j.status="PENDING"');
+
+        $ind = 0;
+
+        while ($row = mysql_fetch_array($result)) {
+            $a = new TranscodingJob();
+            $a = TranscodingJob::_read_row($a, $row);
+
+            $objs[$ind] = $a;
+            $ind++;
+        }
+
+        mysql_free_result($result);
+
+        return $objs;
+    }
+    
+    function fetchRandomPendingJob() {
+        $result = null;
+        $result = _mysql_query('select j.id, j.project_file_id, j.status, j.info, j.entry_date, ' .
+                               'j.updated_date, f.filename, f.project_id ' .
+                               'from pp_transcoding_job j left join pp_project_file f on f.id=j.project_file_id ' .
+                               'where j.status="PENDING" LIMIT 0,1');
+        
+        $row = mysql_fetch_array($result);
+        $a = null;
+        if ($row) {
+            $a = new TranscodingJob();
+            $a = TranscodingJob::_read_row($a, $row);
+        }
+        mysql_free_result($result);
+
+        return $a;
+    }
+  
+    function _read_row($a, $row) {
+        $a->id               = $row['id'];
+        $a->status           = $row['status'];
+        $a->projectFileId    = $row['project_file_id'];
+        $a->info             = $row['info'];
+        $a->entryDate        = reformat_sql_date($row['entry_date']);
+        $a->updateDate       = reformat_sql_date($row['updated_date']);
+        $a->projectId        = $row['project_id'];
+        $a->filename         = $row['filename'];
+        return $a;
+    }
+
+    // class functions
+    // ---------------
+    function createTable() {
+        $ok = _mysql_query(
+            'create table if not exists pp_transcoding_job ' .
+            '(' .
+            'id                        int(10)      not null auto_increment, ' .
+            'project_file_id           int(5)       not null, ' .
+            'status                    varchar(20)  not null, ' .
+            'entry_date                datetime     not null default "1970-01-01 00:00:00", ' .
+            'updated_date              datetime             ,' .
+            'info                      varchar(255)         ,' .
+            'primary key (id), ' .
+            'key status (status), ' .
+            'key entry_date (entry_date) ' .
+            ') default charset=utf8'
+        );
+
+        return $ok;
+    }
+
+    function save() {
+        if (isset($this->id)) {
+            return $this->update();
+        } else {
+            return $this->insert();
+        }
+    }
+
+    function insert() {
+        $ok = _mysql_query(
+            'insert into pp_transcoding_job ' .
+            '(project_file_id, status, entry_date, updated_date, info)' .
+            'values (' .
+            n($this->project_file_id)            . ', ' .
+            qq($this->status)                     . ', ' .
+            'now()'                              . ', ' .
+            'now()'                              . ', ' .
+            qq($this->info)                      .
+            ')'
+        );
+
+        if (!$ok) {
+            return false;
+        }
+
+        $this->id = mysql_insert_id();
+
+        return $ok;
+    }
+
+    function update() {
+        $ok = _mysql_query(
+            'update pp_transcoding_job ' .
+            'set project_file_id='         . n($this->projectFileId)            . ', ' .
+            'status = '                     . qq($this->status)                     . ', ' .
+            'updated_date = '              . 'now()'                              . ', ' .
+            'info = '                      . qq($this->info)                      . ' ' .
+            'where id = '                  . n($this->id)
+        );
+        return $ok;
+    }
+    
+    function updateStatusBasedOnOldStatus($oldStatus) {
+        $ok = _mysql_query(
+            'update pp_transcoding_job ' .
+            'set status = '    . qq($this->status) . ', ' .
+            'updated_date = '  . 'now()'           . ' ' .
+            'where id=' . n($this->id) . ' and status=' . qq($oldStatus)
+        );
+        $affectedRows = mysql_affected_rows();
+        $resp = array('ok'=>$ok, 'affectedRows'=>$affectedRows);
+        return $resp;
+    }
+}
+
+?>
---
+++ Transcoding/TranscodingJobProcessor.php
@@ -0,0 +1,162 @@
+<?php
+
+set_time_limit(120); // 2 minutes hard limit
+
+include_once('../Includes/Init.php');
+include_once('../Includes/Config.php');
+
+include_once('../Includes/DbConnect.php');
+include_once('../Includes/DB/Project.php');
+include_once('../Includes/DB/ProjectFile.php');
+include_once('../Includes/DB/TranscodingJob.php');
+
+$startTime = time();
+
+// $logger->set_debug_level();
+
+// check if this script is currently running
+$processingFile = '../Tmp/processing';
+
+exitIfValidProcessingFileFound($processingFile);
+
+$logger->debug('touching processing file');
+touch($processingFile);
+chmod($processingFile, 0666);
+
+doTheWork($startTime);
+
+$logger->debug('removing processing file');
+unlink($processingFile);
+
+// END
+
+// functions
+function doTheWork($startTime) {
+    global $logger;
+
+    // php script startet und schreibt ein processing file
+    // dann macht es seine arbeit
+    // entweder ists vor einer minute fertig, dann sleept es und sucht nach neuen jobs, bis die minute fertig ist
+    // oder es läuft länger als eine minute, dann hörts nach dem gerade aktuellen render prozess auf
+    // beim verlassen wird das processing file gelöscht
+
+    // FIXME + optimieren: wenn ein worker etwas länger als eine minute arbeitet und dann aufhört, dann kann
+    // eine wartezeit von fast einer ganzen minute entstehen.
+
+    while (true) {
+        if (time() - $startTime > 58) {
+            $logger->debug('it\'s time to quit now so that cron can start this script again');
+            return;
+        }
+
+        // get the next TranscodingJob record with status PENDING
+        $pjob = fetchAndLockNextPendingJob();
+
+        // process it
+        if ($pjob) {
+            transcode($pjob);
+        }
+
+        // sleep
+        usleep(500000); // 0.5 seconds
+    }
+}
+
+
+function transcode(&$pjob) {
+    global $logger;
+
+    $logger->info('transcoding file ' . $pjob->filename . ' (jobId = ' . $pjob->id . ')');
+
+    try {
+    
+        $sourceFile = $GLOBALS['CONTENT_BASE_PATH'] . $pjob->filename;
+        $mp3Filename = getFilenameWithoutExt($pjob->filename) . '.mp3';
+        $destFile = $GLOBALS['CONTENT_BASE_PATH'] . $mp3Filename; 
+    
+        $command = $GLOBALS['TRANSCODER_COMMAND'] . ' ' . $sourceFile . ' ' . $destFile;
+    
+        // execute the command
+        $output = array();
+        $returnVar = 0;
+        $logger->debug('executing command: ' . $command);
+        $ret = exec($command, $output, $returnVar);     
+        
+        if ($returnVar != 0) {
+            throw new Exception('Failed to transcode file, command returned: ' . $ret);
+        }            
+        
+        // clone the existing project file
+        $newProjectFile = ProjectFile::fetch_for_id($pjob->projectFileId);
+        // edge case, when the projectFile does not exist the fetch_for_id returns
+        // and empty ProjectFile instance
+        if ($newProjectFile->id == $pjob->projectFileId) {
+            $newProjectFile->id = null;
+            $newProjectFile->filename = $mp3Filename;
+            $newProjectFile->autocreated = 1;
+            $success = $newProjectFile->save();
+            if (!success) {
+                throw new Exception('Failed to create new ProjectFile db entry!');   
+            }                                             
+        } else {
+            throw new Exception('No ProjectFile found (id: ' . $pjob->projectFileId . ')!');           
+        }
+
+        
+        $pjob->status = 'SUCCESS';
+        $pjob->update();
+
+    } catch (Exception $e) {
+        $logger->error('Exception occured: ' . $e->getMessage());
+        $pjob->info = $e->getMessage();
+        $pjob->status = 'FAILED';
+        $pjob->update();
+    }
+}
+
+function fetchAndLockNextPendingJob() {
+    global $logger;
+
+    //$pjob = PageRenderingJob::fetchOldestPendingJob();
+    $pjob = TranscodingJob::fetchRandomPendingJob();
+
+    if ($pjob) {
+        // attempt to lock the job by updating the status
+        $pjob->status = 'PROCESSING';
+        $resp = $pjob->updateStatusBasedOnOldStatus('PENDING');
+
+        if ($resp['ok']) {
+            if ($resp['affectedRows'] > 0) {
+                return $pjob;
+            } else {
+                $logger->info('job could not be locked because another worker grabbed it before i could lock it -> trying again');
+                return fetchAndLockNextPendingJob();
+            }
+
+        } else {
+            $logger->error('failed to lock job!');
+            return null;
+        }
+
+    } else {
+        $logger->debug('no pending jobs found');
+        return null;
+    }
+}
+
+function exitIfValidProcessingFileFound($processingFile) {
+    global $logger;
+
+    if (file_exists($processingFile)) { // there's a file, the script seems to be running right now
+        if (time() - filemtime($processingFile) < 60) { // we exit if the file is younger than a minute, otherwise we assume that the previous process died
+            $logger->debug('valid processing file found, exit ...');
+            exit;
+        }
+    }
+}
+
+function getFilenameWithoutExt($filename) {  
+    return pathinfo($filename, PATHINFO_FILENAME); 
+} 
+
+?>

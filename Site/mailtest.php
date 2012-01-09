c489c09b614c7b60c08c66228004d5c3b45f0050
Hanno Jonas <hanno.jonas@gmail.com>
2012-01-09 11:37:35 +0100

    test upd

--- Site/mailtest.php
+++ Site/mailtest.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL | E_STRICT);
 
 include_once('../Includes/Config.php');
 
+// FIXME - make sure this is in the config!
 $GLOBALS['MAIL_FROM_NAME'] = 'noreply-oneloudr';
 $GLOBALS['MAIL_FROM_ADDRESS'] = 'noreply@oneloudr.com';
 $GLOBALS['MAIL_REPLY_TO_ADDRESS'] = 'noreply@oneloudr.com';
@@ -14,12 +15,12 @@ $GLOBALS['EMAIL_DELIVERY_OVERRIDE_ALLOWED_RECIPIENTS'] = array();
 $GLOBALS['EMAIL_DELIVERY_OVERRIDE_ADDR'] = 'hjonas@gmx.at';
 $GLOBALS['BCC_EMAIL_ADDRESS'] = '';
 $GLOBALS['MAIL_SERVER_HOST'] = 'smtp.sendgrid.net';
-$GLOBALS['MAIL_SERVER_PORT'] = 25;
+$GLOBALS['MAIL_SERVER_PORT'] = 587;
 $GLOBALS['MAIL_SERVER_AUTH_TYPE'] = 'basic';
 $GLOBALS['MAIL_SERVER_AUTH_USER'] = 'oneloudr';
 $GLOBALS['MAIL_SERVER_AUTH_PWD'] = 'oneloudr';
 
-include_once('../Includes/MailUtil.php');
+include_once('../Includes/Mailer/MailUtil.php');
 
 $msg = "hey, \n\njust a quick test if this whole email delivery thing really works\nin a reliable way ... cu,\n the test script.";
 
@@ -31,12 +32,10 @@ $recipients = array(
   'hanno.jonas@vol.at'
 );
 
-foreach ($recipients as $rec) {
-  sendEmail(
-    $rec,
+sendEmailToRecipients(
+    $recipients,
     'subject',
     $msg 
-  );
-}
+);
 
 ?>
